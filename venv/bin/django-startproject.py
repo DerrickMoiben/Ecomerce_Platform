c4edb19e27988529638210dfa728fbce7328fb91
derrickkimtai <derrickmoio92@gmail.com>
2025-03-13 18:53:32 +0300

    starting the ecomerce platfrom by installing all required apps and projects

---
+++ venv/bin/django-startproject.py
@@ -0,0 +1,9 @@
+#!/home/kim/Projects/Ecomerce_Platform-1/venv/bin/python3
+
+def main():
+    from django_startproject.management import start_project
+    start_project()
+
+
+if __name__ == '__main__':
+    main()
