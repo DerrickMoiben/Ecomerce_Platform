af6ceca505f61b9b7e37009d6ad1356f10e5dc3f
derrickkimtai <derrickmoio92@gmail.com>
2025-03-26 17:04:02 +0300

    added an inventory now

--- myvolkswagen/customer/urls.py
+++ myvolkswagen/customer/urls.py
@@ -6,6 +6,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('landingpage/', views.landingpage, name='landingpage'),
+    path('display_products/', views.display_products, name='display_products'),
+    path('product_details/<int:product_id>/', views.product_details, name='product_details'),
+    path('product_payment/<int:product_id>/', views.product_payment, name='product_payment'),
 ]
 
 if settings.DEBUG:
--- myvolkswagen/customer/views.py
+++ myvolkswagen/customer/views.py
@@ -1,6 +1,23 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from owner.models import Product, ProductImage
 
 
 def landingpage(request):
     return render(request, 'landingpage.html')
+
+
+def display_products(request):
+    products = Product.objects.all()
+    return render(request, 'display_products.html', {'products': products})
+
+def product_details(request,  product_id):
+    product = Product.objects.get(id=product_id)
+    images = ProductImage.objects.filter(product=product)
+    return render(request, 'product_details.html', {'product': product, 'images': images})
+
+
+def product_payment(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_payment.html', {'product': product})
+    
